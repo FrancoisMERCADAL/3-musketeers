52982056148322796afe5529cdb3936f60b17c46
FrancoisMERCADAL <43736293+FrancoisMERCADAL@users.noreply.github.com>
2019-03-05 02:31:03 +0100

    Add files via upload

--- starwars/src/index.test.js
+++ starwars/src/index.test.js
@@ -1,3 +1,5 @@
+//npm run test
+
 const starWars = require('./index');
 const starWarsNames = require('./starwars-names.json');
 
@@ -7,25 +9,35 @@ describe('starwars-names', () => {
       expect(starWars.all).toHaveLength(starWarsNames.length)
     });
     test('should be an array of strings', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      for(var i = 0; i < starWarsNames.length; i++){
+        expect(starWarsNames[i]).toMatch(/.+/);
+      }
     });
 
     test('should contain `Luke Skywalker`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).toContain('Luke Skywalker');
     });
 
     test('should not contain `Ben Quadinaros`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).not.toContain('Ben Quadinaros');
     });
   });
 
   describe('random', () => {
     test('should return a random item from the starWars.all', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      let random = Math.floor(Math.random() * Math.floor(starWars.all.length));
+      expect(starWars.all[random]);
     });
 
-    test('should return an array of random items if passed a number', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+    test('should return an array of random items if passed a number', (entry) => {
+      let list = [];
+      if(parseInt(entry, 10)){
+        for(let i = 0; i < starWars.all.length; i++) {
+          let random = Math.floor(Math.random() * Math.floor(starWars.all.length));
+          list.push(starWars.all[random]);
+          expect(list[i]).toMatch(starWars.all[random]);
+        }
+      }
     });
   });
 });
